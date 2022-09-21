3c3ae5ac05d7a1ac8bcb06c72041fcfc53a80afb
Guille <guille1000142@gmail.com>
2022-09-21 17:01:56 +0200

    railway

--- index.js
+++ index.js
@@ -4,22 +4,35 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const helmet = require("helmet");
 const jwt = require("jsonwebtoken");
 const { db } = require("./firebase/firebase");
 
 const chat = db.collection("bot");
+const users = db.collection("users");
 const donations = db.collection("donations");
 const predictions = db.collection("predictions");
+const promotions = db.collection("promotions");
 
 var botData = [];
+var usersData = [];
 var donationsData = [];
 var predictionsData = [];
+var promotionsData = [];
 
 const botListener = chat.onSnapshot(
   (docsSnapshot) => {
     const docs = docsSnapshot.docs.map((doc) => doc.data());
     botData = docs;
+  },
+  (err) => {
+    console.log(`Encountered error: ${err}`);
+  }
+);
+
+const usersListener = users.onSnapshot(
+  (docsSnapshot) => {
+    const docs = docsSnapshot.docs.map((doc) => doc.data());
+    usersData = docs;
   },
   (err) => {
     console.log(`Encountered error: ${err}`);
@@ -46,6 +59,16 @@ const predictionsListener = predictions.onSnapshot(
   }
 );
 
+const promotionsListener = promotions.onSnapshot(
+  (docsSnapshot) => {
+    const docs = docsSnapshot.docs.map((doc) => doc.data());
+    promotionsData = docs;
+  },
+  (err) => {
+    console.log(`Encountered error: ${err}`);
+  }
+);
+
 app.get("/", (req, res) => {
   res.send("<h1>Partner3</h1>");
 });
@@ -58,40 +81,48 @@ const io = new Server(server, {
   cors: {
     origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST"],
-    transports: ["websocket", "polling"],
+    transports: ["websokcet", "polling"],
     credentials: true,
   },
   allowEIO3: true,
 });
 
- io.use(function (socket, next) {
-   if (socket.handshake.query && socket.handshake.query.token) {
-     jwt.verify(
-       socket.handshake.query.token,
-       process.env.PRIVATE_KEY,
-       function (err, decoded) {
-         if (err) return next(new Error("Authentication error"));
-         socket.decoded = decoded;
-         next();
-       }
-     );
-   } else {
-     next(new Error("Authentication error"));
-   }
- });
+io.use(function (socket, next) {
+  if (socket.handshake.query && socket.handshake.query.token) {
+    jwt.verify(
+      socket.handshake.query.token,
+      process.env.PRIVATE_KEY,
+      function (err, decoded) {
+        if (err) return next(new Error("Authentication error"));
+        socket.decoded = decoded;
+        next();
+      }
+    );
+  } else {
+    next(new Error("Authentication error"));
+  }
+});
 
 io.on("connection", (socket) => {
+  console.log(socket.id);
+
   socket.emit("receive_bot", botData);
+  socket.emit("receive_users", usersData);
   socket.emit("receive_donations", donationsData);
   socket.emit("receive_predictions", predictionsData);
+  socket.emit("receive_promotions", promotionsData);
 
-  setInterval(() => {
-    socket.emit("receive_donations", donationsData);
-    socket.emit("receive_predictions", predictionsData);
-  }, 5000);
+  // setInterval(() => {
+  //   socket.emit("receive_donations", donationsData);
+  //   socket.emit("receive_predictions", predictionsData);
+  // }, 5000);
 
   socket.on("send_bot", (data) => {
     socket.emit("receive_bot", botData);
+  });
+
+  socket.on("send_users", (data) => {
+    socket.emit("receive_users", usersData);
   });
 
   socket.on("send_donations", (data) => {
@@ -100,6 +131,10 @@ io.on("connection", (socket) => {
 
   socket.on("send_predictions", (data) => {
     socket.emit("receive_predictions", predictionsData);
+  });
+
+  socket.on("send_promotions", (data) => {
+    socket.emit("receive_promotions", promotionsData);
   });
 });
 const PORT = process.env.PORT;
